09bad0fb67309089cf5d65398d04e1bef9fd0f83
Vipul Mhatre <125679638+Star-Viper@users.noreply.github.com>
2023-12-12 22:04:18 +0530

    added 11.3

---
+++ Exp11.3.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+# Vipul Mhatre 60019220025 B062
+
+root = tk.Tk()
+root.title("Christmas Tree")
+
+canvas = tk.Canvas(root, width=400, height=400, bg='white')
+canvas.pack()
+
+canvas.create_polygon(50, 300, 200, 50, 350, 300, fill='green')
+
+canvas.create_rectangle(170, 300, 230, 350, fill='brown')
+
+canvas.create_oval(150, 150, 180, 180, fill='red')
+canvas.create_oval(210, 210, 220, 220, fill='red')
+canvas.create_oval(250, 250, 280, 280, fill='red')
+
+
+root.mainloop()
